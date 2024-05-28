565f4c8b467a6d30823f77dfc7241a00a63128d7
Ishan Madusanka Waduge <ishan46madusanka@gmail.com>
2024-05-28 23:03:15 +0530

    Fixed CSS and JS bugs.

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// For upload image button----------------------------------------------------------------------------
+const actualBtn = document.getElementById('upimg');
+
+const fileChosen = document.getElementById('fileimg');
+
+actualBtn.addEventListener('change', function(){
+  fileChosen.textContent = this.files[0].name
+})
+
+
+
+
+
+//color change unicode button (when Legasy button click)-----------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+  const legacyBtn = document.querySelector('.LegacyBtn');
+  const unicodeBtn = document.querySelector('.UnicodeBtn');
+
+  legacyBtn.addEventListener('focus', () => {
+      unicodeBtn.style.background = '#e4e4e4'; // Change this to your desired color
+  });
+
+  legacyBtn.addEventListener('blur', () => {
+      unicodeBtn.style.background = '#ffffff'; // Change this to the original color
+  });
+
+  legacyBtn.addEventListener('click', () => {
+    unicodeBtn.style.border = '2px #e4e4e4 solid'; // Change this to your desired border color
+});
+
+unicodeBtn.addEventListener('click', () => {
+  unicodeBtn.style.border = '2px #ffffff solid'; // Revert border color to white on click
+});
+});
+
+
+
+
+
+//font change dropdown list--------------------------------------------------------------------------------
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /*when an item is clicked, update the original select box,
+        and the selected item:*/
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
